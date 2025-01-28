c888737c6b9ce10bb60ee9af50890b016dfe0176
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-01-28 17:34:28 -0500

    Changes from 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a programmer!"
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         
         VStack {
             
             Spacer()
             
-            Image(systemName: "swift")
+            Image(systemName: imageName)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
@@ -28,12 +28,17 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    message = "Awesome!"
-                }
-                Button("Great") {
-                    message = "Great!"
+            Button("Press Me!"){
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                if message == message1 {
+                    message = message2
+                    imageName = imageString2
+                } else {
+                    message = message1
+                    imageName = imageString1
                 }
             }
                 .buttonStyle(.borderedProminent)
