8e05a05c116f7fa44c06f73f8b4c9611c3dd8ae1
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-01-28 14:58:55 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Noah McGuire on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
